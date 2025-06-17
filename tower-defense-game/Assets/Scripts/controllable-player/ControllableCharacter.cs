4befae23d59f15f66b048be9924a3828f9b9c3da
kwanyinlai <laikwanyin06@gmail.com>
2025-06-18 03:04:17 +0800

    Fixed movement.
    Camera will need to be readjusted as appropriate (for observer mode and what not) but it works for now.

--- tower-defense-game/Assets/Scripts/controllable-player/ControllableCharacter.cs
+++ tower-defense-game/Assets/Scripts/controllable-player/ControllableCharacter.cs
@@ -98,8 +98,8 @@ public class CharacterMovement : MonoBehaviour
         float horiz = Input.GetAxis("Horizontal");
         float vert = Input.GetAxis("Vertical");
 
-        Vector3 forward = transform.forward;
-        Vector3 right = transform.right;
+        Vector3 forward = overheadCam.transform.forward;
+        Vector3 right = overheadCam.transform.right;
 
         forward.y = 0f;
         right.y = 0f;
@@ -129,9 +129,8 @@ public class CharacterMovement : MonoBehaviour
         
         if (dir != Vector3.zero)
         {
-            dir *= -1;
             Quaternion rotateDir = Quaternion.LookRotation(dir);
-            // transform.rotation = Quaternion.RotateTowards(transform.rotation, rotateDir, angularSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotateDir, angularSpeed * Time.deltaTime);
         }
         
     }

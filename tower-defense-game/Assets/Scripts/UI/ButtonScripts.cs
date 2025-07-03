0fa9fb5421f11425b72e583da2f50d932807c53a
YizhouZhangJackie <jackiezhang2007@gmail.com>
2025-07-03 19:30:49 +0800

    Build Menu Category Selection
    Added categories for build menu to select builds of different categories.

--- tower-defense-game/Assets/Scripts/UI/ButtonScripts.cs
+++ tower-defense-game/Assets/Scripts/UI/ButtonScripts.cs
@@ -1,11 +1,16 @@
+using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ButtonScripts : MonoBehaviour
 {
     private BuildMode buildMode;
+    public List<GameObject> allButtonsPrefabs = new List<GameObject>();
     void Start()
     {
+        //Keep Build Mode enabled at the start of the game so this will not raise a null reference exception
+        //The Build Menu will auto disable upon game start
         buildMode = GameObject.Find("player1").GetComponent<BuildMode>();
     }
 
@@ -36,5 +41,49 @@ public class ButtonScripts : MonoBehaviour
     {
         buildMode.setActiveBuilding(0);
         buildMode.CloseBuildMenu();
+    }
+
+    public void sortAllBuilds()
+    {
+        GameObject options = GameObject.Find("BuildingChoices(Content)");
+        destroyAllChildren(options);
+        for(int i = 0; i < allButtonsPrefabs.Count; i++)
+        {
+            Instantiate(allButtonsPrefabs[i], options.transform);
+        }
+    }
+
+    public void sortBuildsBy(string category)
+    {
+        string filter;
+        switch (category)
+        {
+            case "barrack":
+                filter = "BarrackTypeButton";
+                break;
+            case "wall":
+                filter = "WallTypeButton";
+                break;
+            default:
+                filter = "Error?";
+                break;
+        }
+        GameObject options = GameObject.Find("BuildingChoices(Content)");
+        destroyAllChildren(options);
+        for (int i = 0; i < allButtonsPrefabs.Count; i++)
+        {
+            if (allButtonsPrefabs[i].tag == filter)
+            {
+                Instantiate(allButtonsPrefabs[i], options.transform);
+            }
+        }
+    }
+
+    private void destroyAllChildren(GameObject parent)
+    {
+        foreach (Transform child in parent.transform)
+        {
+            Destroy(child.gameObject);
+        }
     }
 }

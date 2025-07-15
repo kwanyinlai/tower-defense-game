1b5a1d518d69a4e4a812e888ae86d41e0a50ee4a
einsk <laikwanyin06@gmail.com>
2025-07-15 17:32:19 +0800

    Update SnapToGrid.cs

--- tower-defense-game/Assets/Scripts/structures/building-outline/SnapToGrid.cs
+++ tower-defense-game/Assets/Scripts/structures/building-outline/SnapToGrid.cs
@@ -8,7 +8,11 @@ public class SnapToGrid : MonoBehaviour
     private Placeable placeable;
     [SerializeField] private BuildMode buildMode;
     [SerializeField] private GameObject commandingPlayer;
+<<<<<<< Updated upstream
     [SerializeField] private GameObject mainCamera;
+=======
+
+>>>>>>> Stashed changes
 
     private Vector3[] TILEOFFSET = {new Vector3(0f,0f,4f), new Vector3(0f,0f,-4f), 
                                   new Vector3(4f,0f,0f), new Vector3(-4f,0f,0f)};
@@ -34,7 +38,13 @@ public class SnapToGrid : MonoBehaviour
     
     void Update()
     {
-        Vector3 mousePos = GetMousePosOnPlane();
+        Vector3? mouseNullCheck = GetMousePosOnPlane();
+        if (mouseNullCheck==null)
+        {
+            // show red outline
+            return;
+        }
+        Vector3 mousePos = (Vector3)mouseNullCheck;
         transform.position = GridSystem.GridToCoordinates(GridSystem.CoordinatesToGrid(
             mousePos));
         // for this model, the centre of the model is off for some reason so it looks wrong. i'm pretty sure
@@ -51,10 +61,11 @@ public class SnapToGrid : MonoBehaviour
         else{
             if(!placeable.IsBuildable(gameObject.transform.position)){
                 
-                if ( NearestGrid() !=null){
+                if ( NearestGrid() != null){
                     transform.position = (Vector3) NearestGrid();
                     return;
                 }
+
             }
 
             transform.localScale=new Vector3(0f,0f,0f);
@@ -66,32 +77,39 @@ public class SnapToGrid : MonoBehaviour
     Vector3? NearestGrid(){
  
         float[] distances = new float[4];
-        for(int i = 0 ; i<4 ; i++){
+
+        Vector3 mousePos = (Vector3)GetMousePosOnPlane();
+        if (mousePos == null) {
+            return null;
+        }
+        for (int i = 0 ; i<4 ; i++){
             if(placeable.IsBuildable( transform.position + TILEOFFSET[i] ) ){
-                distances[i] = Vector3.Distance(GetMousePosOnPlane(), transform.position + TILEOFFSET[i]);
+
+                distances[i] = Vector3.Distance(mousePos, transform.position + TILEOFFSET[i]);
             }
             else{
-                distances[i] = float.MaxValue; 
+                distances[i] = float.MaxValue;
+
             }
            
 
-        } 
-        float minDistance = distances.OrderBy(n => n) 
-                                    .Distinct()
-                                    .FirstOrDefault();
+        }
+        float minDistance = distances.OrderBy(n => n).First();
         if(minDistance == float.MaxValue){
+            Debug.Log("why are we here");
             return null;
         }
         int ind = Array.IndexOf(distances, minDistance);
         
         if(ind==-1){
+            Debug.Log("why are we here2");
             return null;
         }
         return transform.position + TILEOFFSET[ind];
 
     }
 
-    Vector3 GetMousePosOnPlane()
+    Vector3? GetMousePosOnPlane()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
@@ -101,6 +119,6 @@ public class SnapToGrid : MonoBehaviour
            return hit.point; 
             
         }
-        return Vector3.zero;
+        return null;
     }
 }

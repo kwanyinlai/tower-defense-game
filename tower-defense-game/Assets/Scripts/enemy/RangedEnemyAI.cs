e0e4f7e2c04d27d85ae6425ba22aae84b2b988bd
phomebrew <136415283+phomebrew@users.noreply.github.com>
2025-07-21 12:42:21 -0800

    Basic Type now Ranged Type (And hitscan now)
    Renamed the "basic" troop/enemy into a more proper name, the "ranged" troop/enemy
    Made ranged troop/enemy a hitscan (aka no more projectile prefab)

--- tower-defense-game/Assets/Scripts/enemy/RangedEnemyAI.cs
+++ tower-defense-game/Assets/Scripts/enemy/RangedEnemyAI.cs
@@ -9,25 +9,16 @@ public class RangedEnemyAI : EnemyAI
     public GameObject bulletPrefab;
     private HashSet<string> exceptionBulletList = new HashSet<string>{"Enemy"};
     
-    public override void Attack(Transform bulletTarget)
+    public override void Attack(Transform target)
     {
 
-        transform.LookAt(bulletTarget); 
+        transform.LookAt(target); 
         transform.rotation = new Quaternion(0, transform.rotation.y, 0, transform.rotation.w); // essentially only allows the y axis to move
-
         
-        Collider collider = this.GetComponent<Collider>();
-        Vector3 bulletPos = transform.position + new Vector3(0f, collider.bounds.size.y * (0.75f), 0); // Adds Vector For Y Axis To Shift Bullet To Head Level
-        // bulletPos.y += collider.bounds.size.y * (0.75f);
-        if (bulletPrefab != null)
+        CombatSystem combatSystem = target.GetComponent<CombatSystem>(); // interacts directly with the target rather than creating a projectile
+        if (combatSystem != null)
         {
-            GameObject bullet = Instantiate(bulletPrefab, bulletPos, transform.rotation);
-            BulletScript bulletScript = bullet.GetComponent<BulletScript>();
-            if (bulletScript != null)
-            {
-                bulletScript.SetExceptionList(exceptionBulletList);
-                bulletScript.SetTarget(bulletTarget);
-            }
+            combatSystem.TakeDamage((int)(dmg * (1 + combatSystem.GetEffectStrength("attackBuff") - combatSystem.GetEffectStrength("attackWeaken"))));
         }
         atkTimer = atkCooldown;
     }
--- tower-defense-game/Assets/Scripts/player_troops/RangedTroopAI.cs
+++ tower-defense-game/Assets/Scripts/player_troops/RangedTroopAI.cs
@@ -10,20 +10,13 @@ public class RangedTroopAI : TroopAI
         transform.LookAt(enemyTarget);
         transform.rotation = new Quaternion(0, transform.rotation.y, 0, transform.rotation.w); // essentially only allows the y axis to move
         
-        Vector3 bulletPos = transform.position;
-        Collider collider = this.GetComponent<Collider>();
-        bulletPos.y += collider.bounds.size.y * (0.75f);
-        if (bulletPrefab != null)
+        CombatSystem combatSystem = enemyTarget.GetComponent<CombatSystem>(); // interacts directly with the target rather than creating a projectile
+        if (combatSystem != null)
         {
-            GameObject bullet = Instantiate(bulletPrefab, bulletPos, transform.rotation);
-            BulletScript bulletScript = bullet.GetComponent<BulletScript>();
-            if (bulletScript != null)
-            {
-                bulletScript.SetExceptionList(exceptionBulletList);
-                bulletScript.SetTarget(enemyTarget);
-            }
+            combatSystem.TakeDamage((int)(dmg * (1 + combatSystem.GetEffectStrength("attackBuff") - combatSystem.GetEffectStrength("attackWeaken"))));
         }
         atkTimer = atkCooldown;
+
     }
 
     protected override void IntializeSellResources()

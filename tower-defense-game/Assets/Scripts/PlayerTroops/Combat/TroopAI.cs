e13ea6400ef3abc33d46a88ea2ff081a64883784
kwanyinlai <laikwanyin06@gmail.com>
2025-10-10 14:13:31 -0400

    fixed broken getter setters (can't use get; set; shortcut for protected attributes probably

--- tower-defense-game/Assets/Scripts/PlayerTroops/Combat/TroopAI.cs
+++ tower-defense-game/Assets/Scripts/PlayerTroops/Combat/TroopAI.cs
@@ -13,37 +13,37 @@ public abstract class TroopAI : MonoBehaviour
 
     [Header("Troop Attributes")]
     [SerializeField] protected string troopName;
-    public string TroopName{ get; }
+    public string TroopName{ get { return troopName; } }
     [SerializeField] protected GameObject troopModel;
-    public GameObject TroopModel{ get; set; }
+    public GameObject TroopModel{ get{ return troopModel; } set { troopModel = value; } }
     [SerializeField] protected float maxSpeed = 3.5f;
-    public float MaxSpeed { get; }
+    public float MaxSpeed { get{ return maxSpeed; } }
 
     [Header("Combat Attributes")]
     [SerializeField] protected float attackRange = 5.0f ;
-    public float AttackRange{ get; set; }
+    public float AttackRange{ get{ return attackRange; } set{ attackRange = value; } }
     [SerializeField] protected int dmg = 10;
     public int Damage
     {
         get { return dmg; }
-        set { this.dmg = value; }
+        set { dmg = value; }
     }
     [SerializeField] protected float aggroRange = 10.0f;
-    public float AggroRange { get; set; } // range which troop engages enemy
+    public float AggroRange { get{ return attackRange; } set{ attackRange = value; } } // range which troop engages enemy
     [SerializeField] protected Transform enemyTarget;
-    public Transform EnemyTarget { get; set; }
+    public Transform EnemyTarget { get{ return enemyTarget; } set{ enemyTarget = value; } }
 
     [Header("Control Attributes")]
 
     protected bool isUnderSelection = false;
-    public bool IsUnderSelection { get; set; }
+    public bool IsUnderSelection { get{ return isUnderSelection; } set{ isUnderSelection = value; } }
 
     [SerializeField] protected GameObject commandingPlayer;
-    public GameObject CommandingPlayer { get; set; }
+    public GameObject CommandingPlayer { get{ return commandingPlayer; } set{ commandingPlayer = value; } }
     [SerializeField] protected GameObject waypoint;
-    public GameObject Waypoint { get; set; } // maybe these shouldn't be public?
-    [SerializeField] protected GameObject selectedCircle;
-    public GameObject SelectedCircle { get; set; }
+    public GameObject Waypoint { get{ return waypoint; } set{ waypoint = value; } } // TOOD: maybe protected can't use the getter setter shortcuts
+    [SerializeField] protected GameObject selectedCircle; // TODO: change build menu isBuilding to more descriptive
+    public GameObject SelectedCircle { get{ return selectedCircle; } set{ selectedCircle = value; } }
     
 
     // PRIVATE AND PROTECTED ATTRIBUTES 

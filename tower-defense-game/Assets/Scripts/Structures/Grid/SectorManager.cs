bc7f7a10789a0980fc1e885703a1ae47e78dd9f3
kwanyinlai <laikwanyin06@gmail.com>
2026-01-17 00:44:56 -0500

    clean up sector manager with reduced responsibilities for flowfield managment

--- tower-defense-game/Assets/Scripts/Structures/Grid/SectorManager.cs
+++ tower-defense-game/Assets/Scripts/Structures/Grid/SectorManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.Mathematics;
 
 // TODO: STORE A GLOBAL LIST TO THE BASE FOR ENEMIES WHEN LEVEL STARTS
+// TODO: maybenot
 
 public class SectorManager : MonoBehaviour
 {
@@ -22,7 +23,7 @@ public class SectorManager : MonoBehaviour
 
     private GridSector[,] sectors;
 
-    // Cardinal direction offsets (North, East, South, West)
+    // cardinal direction offsets
     private static readonly int2[] DIRECTIONS = {
         new int2(0, 1),   // North
         new int2(1, 0),   // East
@@ -32,7 +33,7 @@ public class SectorManager : MonoBehaviour
 
     #region Initialization
 
-    public void SetSectors(GridSector[,] sectors)
+    public void SetSectorList(GridSector[,] sectors)
     {
         this.sectors = sectors;
     }
@@ -41,115 +42,10 @@ public class SectorManager : MonoBehaviour
 
     #region Sector Connectivity
 
-    /// <summary>
-    /// Checks if two adjacent nodes can be traversed between
-    /// </summary>
-    private bool AreNodesConnected(GridNode a, GridNode b)
-    {
-        return a.walkCost < float.MaxValue && b.walkCost < float.MaxValue;
-    }
-
-    /// <summary>
-    /// Determines if two sectors are adjacent
-    /// </summary>
     public bool AreSectorsAdjacent(GridSector sector1, GridSector sector2)
     {
-        int manhattanDistance = Mathf.Abs(sector1.sectorCoordinate.x - sector2.sectorCoordinate.x) +
-                                Mathf.Abs(sector1.sectorCoordinate.y - sector2.sectorCoordinate.y);
-        return manhattanDistance == 1;
-    }
-
-    /// <summary>
-    /// Checks if all border nodes between two sectors are traversable
-    /// </summary>
-    /// <param name="from">Source sector</param>
-    /// <param name="to">Target sector</param>
-    /// <param name="direction">Direction from source to target (0=North, 1=East, 2=South, 3=West)</param>
-    /// <returns>True if sectors are fully connected along the border</returns>
-    private bool ComputeConnectivity(GridSector from, GridSector to, int direction)
-    {
-        switch (direction)
-        {
-            case (int)GridSector.CardinalDirections.North:
-                return CheckNorthBorderConnectivity(from, to);
-
-            case (int)GridSector.CardinalDirections.East:
-                return CheckEastBorderConnectivity(from, to);
-
-            case (int)GridSector.CardinalDirections.South:
-                return CheckSouthBorderConnectivity(from, to);
-
-            case (int)GridSector.CardinalDirections.West:
-                return CheckWestBorderConnectivity(from, to);
-
-            default:
-                Debug.LogError($"Invalid direction: {direction}");
-                return false;
-        }
-    }
-
-    private bool CheckNorthBorderConnectivity(GridSector from, GridSector to)
-    {
-        // Check top edge of 'from' sector against bottom edge of 'to' sector
-        for (int x = 0; x < GridSector.sectorWidth; x++)
-        {
-            GridNode fromNode = from.localGrid[x, GridSector.sectorHeight - 1];
-            GridNode toNode = to.localGrid[x, 0];
-
-            if (!AreNodesConnected(fromNode, toNode))
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private bool CheckEastBorderConnectivity(GridSector from, GridSector to)
-    {
-        // Check right edge of 'from' sector against left edge of 'to' sector
-        for (int y = 0; y < GridSector.sectorHeight; y++)
-        {
-            GridNode fromNode = from.localGrid[GridSector.sectorWidth - 1, y];
-            GridNode toNode = to.localGrid[0, y];
-
-            if (!AreNodesConnected(fromNode, toNode))
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private bool CheckSouthBorderConnectivity(GridSector from, GridSector to)
-    {
-        // Check bottom edge of 'from' sector against top edge of 'to' sector
-        for (int x = 0; x < GridSector.sectorWidth; x++)
-        {
-            GridNode fromNode = from.localGrid[x, 0];
-            GridNode toNode = to.localGrid[x, GridSector.sectorHeight - 1];
-
-            if (!AreNodesConnected(fromNode, toNode))
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private bool CheckWestBorderConnectivity(GridSector from, GridSector to)
-    {
-        // Check left edge of 'from' sector against right edge of 'to' sector
-        for (int y = 0; y < GridSector.sectorHeight; y++)
-        {
-            GridNode fromNode = from.localGrid[0, y];
-            GridNode toNode = to.localGrid[GridSector.sectorWidth - 1, y];
-
-            if (!AreNodesConnected(fromNode, toNode))
-            {
-                return false;
-            }
-        }
-        return true;
+        return Mathf.Abs(sector1.sectorCoordinate.x - sector2.sectorCoordinate.x) + 
+            Mathf.Abs(sector1.sectorCoordinate.y - sector2.sectorCoordinate.y) == 1;
     }
 
     #endregion
@@ -309,6 +205,6 @@ public class SectorManager : MonoBehaviour
         float worldZ = (sectorY * GridSector.sectorHeight - GridManager.GRID_HEIGHT / 2) * GridManager.TILE_SIZE;
         return new Vector3(worldX, 0f, worldZ);
     }
-
+    
     #endregion
 }
